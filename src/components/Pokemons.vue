e313d1f814a7fe629b15b76075219893d94ffc6e
Dani <daniela.vilcazuniga@gmail.com>
2024-11-27 00:18:11 -0300

    modifique algo

---
+++ src/components/Pokemons.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  props: ["pokemon", "index"],
+  
+};
+</script>
+
+<template>
+  <div class="pokemon">
+    <img :src="pokemon.img" alt="uno de los mil pokémones" :class="{ filter: !pokemon.nitido }" class="img-poke"/>
+    
+    <p v-show="pokemon.nitido">{{ pokemon.name }}</p>
+    <input v-show="!pokemon.nitido" @keyup.enter="$emit('pokemonEncontrado', index)" type="text" v-model="pokemon.input"/>
+    <button class="my-2" v-show="!pokemon.nitido" @click="$emit('pokemonEncontrado', index)"> Descubrir </button>
+  </div>
+</template>
+
+<style scoped>
+.pokemon{
+  display: flex;
+  flex-direction: column;
+}
+
+.filter {
+  filter: grayscale(100%) blur(5px);
+}
+
+.img-poke {
+  height: 100px;
+}
+
+button {
+  border: none;
+  background-color: #1c2b5e;
+  color: #fff;
+  border-radius:20px;
+  padding:.5rem;
+}
+
+p {
+  text-align: center;
+}
+</style>
